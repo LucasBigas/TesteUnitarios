dd18b0b9bb28a2bf41756413653e757b72263373
Lucas Bigas <112009885+LucasBigas@users.noreply.github.com>
2024-12-02 14:02:54 -0300

    Update SistemaMonitoramentoAvancadoTest.java

--- src/test/java/com/example/CT007/SistemaMonitoramentoAvancadoTest.java
+++ src/test/java/com/example/CT007/SistemaMonitoramentoAvancadoTest.java
@@ -12,38 +12,31 @@ public class SistemaMonitoramentoAvancadoTest {
         SensorBiometrico oximetro = new Oximetro();
         SensorBiometrico termometro = new Termometro();
         SensorBiometrico monitorCardiaco = new MonitorCardiaco();
-
         sistema.conectarSensor(oximetro);
         sistema.conectarSensor(termometro);
         sistema.conectarSensor(monitorCardiaco);
-
         // Atualizar dados
         sistema.atualizarDadosEmTempoReal(oximetro, 95.0);
         sistema.atualizarDadosEmTempoReal(termometro, 36.5);
         sistema.atualizarDadosEmTempoReal(monitorCardiaco, 72.0);
-
         // Validar os dados coletados
         assertEquals(95.0, sistema.getDadoSensor("Oxímetro"));
         assertEquals(36.5, sistema.getDadoSensor("Termômetro"));
         assertEquals(72.0, sistema.getDadoSensor("Monitor Cardíaco"));
     }
-
     @Test
     public void testValidacaoDadosInvalidos() {
         SistemaMonitoramentoAvancado sistema = new SistemaMonitoramentoAvancado();
         SensorBiometrico oximetro = new Oximetro();
-        
         sistema.conectarSensor(oximetro);
         // Dados inválidos
         sistema.atualizarDadosEmTempoReal(oximetro, 60.0);
         assertEquals(60.0, sistema.getDadoSensor("Oxímetro"));
     }
-
     @Test
     public void testSensorInativo() {
         SistemaMonitoramentoAvancado sistema = new SistemaMonitoramentoAvancado();
         SensorBiometrico oximetro = new Oximetro();
-        
         sistema.conectarSensor(oximetro);
         // Sensor inativo
         sistema.verificarSensoresInativos();
